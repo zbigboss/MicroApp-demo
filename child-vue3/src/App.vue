7306c132c79dbb8d8c40940616b83bed9b36b6f1
Denise <denise@decimalpoint.com.cn>
2022-06-08 17:28:55 +0800

    feat: 修改子应用vue3 App.vue文件

--- child-vue3/src/App.vue
+++ child-vue3/src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id=vue3-app>
+    <div id='public-links' @click="onRouteChange">
+      <router-link to="/" page-path=''>Home</router-link> |
+      <router-link to="/page2" page-path='/page2'>Page2</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+<script lang="ts">
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    // 子应用内部跳转时，通知侧边栏改变菜单状态
+    onRouteChange () {
+      if (window.__MICRO_APP_ENVIRONMENT__) {
+        // 发送全局数据，通知侧边栏修改菜单展示
+        window.microApp.setGlobalData({ name: window.__MICRO_APP_NAME__ })
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+<style lang="scss">
+#vue3-app {
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
